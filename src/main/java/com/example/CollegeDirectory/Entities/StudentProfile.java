e9d725536e73f29430d55602d5edec0aa9064eef
yasirfaraz-786 <yasirfaraz27@gmail.com>
2024-10-14 21:00:47 +0530

    first commit

---
+++ src/main/java/com/example/CollegeDirectory/Entities/StudentProfile.java
@@ -0,0 +1,69 @@
+package com.example.CollegeDirectory.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class StudentProfile {
+    @Id
+    private Long userId;
+    private Long id;
+
+    private String photo;
+    private String year;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
+
+    @OneToOne
+    @MapsId
+    private User user;
+
+    // Getters and Setters
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
